571654db07fcfd2b7ce9feec9a0c6834d5a3351b
Kevn Cornl <123711097+codekvnnn@users.noreply.github.com>
2023-07-16 15:47:19 -0400

    Add files via upload
    initial commit

---
+++ controllers/author.controller.js
@@ -0,0 +1,77 @@
+const Author = require("../models/authors.model");
+
+
+
+//c
+const createNewAuthor = (req, res) => {
+    Author.create(req.body)
+        .then((newAuthor) => {
+            res.json({ newAuthor });
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
+};
+
+
+//r
+
+
+const getAllAuthors = (req, res) => {
+    Author.find()
+        .then((allAuthors) => {
+            res.json(allAuthors);
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
+};
+
+const getOneAuthor = (req, res) => {
+    Author.findOne({ _id: req.params.id })
+        .then((queriedAuthor) => {
+            res.json(queriedAuthor);
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
+};
+
+
+// u
+
+
+const updateAuthor = (req, res) => {
+    Author.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+        .then((updatedAuthor) => {
+            res.json({ updatedAuthor });
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
+};
+
+
+// d
+
+
+const deleteExistingUser = (req, res) => {
+    Author.deleteOne({ _id: req.params.id })
+        .then((deletedResponse) => {
+            res.json({ deletedResponse });
+        })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
+};
+
+module.exports = {
+    createNewAuthor,
+    getOneAuthor,
+    getAllAuthors,
+    updateAuthor,
+    deleteExistingUser,
+};
